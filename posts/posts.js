520beb386323cae441e3772ec3b4201bab91c0bd
Biniam Kahsay <Biniamk10@gmail.com>
2024-06-25 17:00:33 -0500

    Create layout for registration page and style the page

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,12 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+window.onload
+{
+    const logoutbutton = document.querySelector("#logout");
+    logoutbutton.addEventListener("click", (event) =>{
+        event.preventDefault();
+        logout();
+    })
+}
