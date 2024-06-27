ede0c5554a58d2d38396449be7a0f08ba7494432
Biniam Kahsay <Biniamk10@gmail.com>
2024-06-27 16:02:04 -0500

    finsh post page

--- posts/posts.js
+++ posts/posts.js
@@ -2,11 +2,72 @@
 
 "use strict";
 
-window.onload
-{
-    const logoutbutton = document.querySelector("#logout");
+window.onload = () => {
+
+    getPosts();
+
+  const logoutbutton = document.querySelector("#logout");
     logoutbutton.addEventListener("click", (event) =>{
         event.preventDefault();
         logout();
     })
+}
+
+
+const getPosts = async () => {
+    try {
+
+        const loginData = getLoginData();
+
+        const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/",{
+            method: "GET",
+            headers: {
+
+                Authorization: `Bearer ${loginData.token}`
+            }
+        })
+
+        const data = await response.json();
+
+        const postsContainer = document.getElementById('postsContainer');
+
+        for (const post of data) {
+            const postElement = document.createElement('div');
+            postElement.className = 'post';
+
+            const postContent = `
+            <h3>${post.text}</h3>
+            <p class="author"><strong>User:</strong> ${post.username}</p>
+            <p class="Timestamp"><strong>Time:</strong> ${new Date(post.createdAt).toLocaleString()}</p>
+            <p> <span class="material-symbols-outlined">
+            </span> </p>
+             `;
+
+            postElement.innerHTML = postContent;
+            postsContainer.appendChild(postElement);
+        }
+    } catch (error) {
+    
+    }
+
+}
+
+function logout() {
+    const loginData = getLoginData();
+
+    const options = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+
+    fetch(apiBaseURL + "/auth/logout", options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .finally(() => {
+
+            window.localStorage.removeItem("login-data");
+            window.location.assign("/");
+        });
 }
